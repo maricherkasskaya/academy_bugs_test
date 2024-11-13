6a694e3ec0feac8b52086a6465dc7f80bc85d158
Maria Cherkasskaya <maria@MacBook-Pro-Maria.local>
2024-11-13 21:20:11 +0300

    update base page

--- src/pages/base.page.js
+++ src/pages/base.page.js
@@ -3,6 +3,8 @@ export class BasePage {
         this.page = page;
     }
     async open (url){
+        await allure.step ("Переход в сервис", async () => {
         await this.page.goto(url);
+    });
     }
 }
